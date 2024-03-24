a7afa78c57404ba5f2602d05c46769d4c922b88c
suzuki.yuto <suzuki.yuto@nifty.co.jp>
2024-03-24 11:04:52 +0900

    momentjsを削除

--- workspaces/app/src/lib/date/getDayOfWeekStr.ts
+++ workspaces/app/src/lib/date/getDayOfWeekStr.ts
@@ -1,10 +1,7 @@
-import type moment from 'moment-timezone';
-
-const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
-
-export const getDayOfWeekStr = (date: moment.Moment) => {
-  const dayOfWeek = date.day();
-  const dayStr = days.at(dayOfWeek);
+export const getDayOfWeekStr = (date: Date) => {
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
+  const dayOfWeek = date.getDay(); // 0 (Sunday) から 6 (Saturday) までの整数を返す
+  const dayStr = days[dayOfWeek];
   if (dayStr == null) {
     throw new Error('dayOfWeek is invalid');
   }
