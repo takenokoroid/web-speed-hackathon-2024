f9d489b8ab8e37d57f7879c4704c5a86a5829356
suzuki.yuto <suzuki.yuto@nifty.co.jp>
2024-03-24 10:17:51 +0900

    muiを必要な分だけ読み込むように修正

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,40 @@
-import * as Icons from '@mui/icons-material';
+// 必要なアイコンを個別にインポートします。
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CloseIcon from '@mui/icons-material/Close';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
+  // 使用するアイコンのタイプを拡張
+  type: 'Search' | 'FavoriteBorder' | 'Favorite' | 'Close' | 'NavigateNext';
   width: number;
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
-  // eslint-disable-next-line
-  const Icon = Icons[type];
-  return <Icon style={{ color, height, width }} />;
+  // アイコンタイプに基づいて適切なアイコンコンポーネントを動的に選択
+  let Icon;
+  switch (type) {
+    case 'Search':
+      Icon = SearchIcon;
+      break;
+    case 'FavoriteBorder':
+      Icon = FavoriteBorderIcon;
+      break;
+    case 'Favorite':
+      Icon = FavoriteIcon;
+      break;
+    case 'Close':
+      Icon = CloseIcon;
+      break;
+    case 'NavigateNext':
+      Icon = NavigateNextIcon;
+      break;
+    default:
+      throw new Error(`Unsupported icon type: ${type}`);
+  }
+
+  return <Icon style={{ color, height: `${height}px`, width: `${width}px` }} />;
 };
